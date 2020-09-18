00eec230c45a758baf3ccfbffe4b74b102fb43d8
sooryaprakash31 <sooryaprakash.r31@gmail.com>
2020-09-18 18:22:30 +0530

    Fixed bugs in scheduling

--- class_auto.py
+++ class_auto.py
@@ -10,25 +10,40 @@ config.read('config.ini')
 username = config.get('AUTH', 'USERNAME')
 password = config.get('AUTH', 'PASSWORD')
 
-classTime = ["09:15","11:35","14:20"]
+classTime = ["09:20","11:40","19:25"]
 
 class ClassAutomation():
     def __init__(self):
         self.count = 0
         self.findCount()
-        schedule.every().day.at(classTime[self.count]).do(self.initClass)
         while True:
-            schedule.run_pending()
-            sleep(1)
+            if datetime.now().strftime("%H:%M") in classTime:
+                print(datetime.now().strftime("%H:%M"))
+                self.initClass()
+            sleep(30)
 
     def findCount(self):
         currentTime=datetime.now().strftime("%H:%M")
-        currentHour = currentTime.split(":")[0]
+        currentHour = int(currentTime.split(":")[0])
+        currentMin = int(currentTime.split(":")[1])
         for i in classTime:
-            if int(currentHour)<int(i.split(":")[0]):
+            if currentHour==int(i.split(":")[0]) and currentMin<int(i.split(":")[1]):
                 self.count = classTime.index(i)
-            
+                print("Next Class at",classTime[self.count],"Today")
+                break
+            elif currentHour<int(i.split(":")[0]):
+                self.count = classTime.index(i)
+                print("Next Class at",classTime[self.count],"Today")
+                break
+            else:
+                if classTime.index(i)==2:
+                    self.count=0
+                    print("Next Class at",classTime[self.count],"Tomorrow")
+                    break
+                continue
+
     def initClass(self):
+        print("Initiating...")
         self.login()
         className = self.findClass()
         self.driver.find_element_by_xpath("//div[text()='{}']".format(className)).click()
@@ -38,11 +53,14 @@ class ClassAutomation():
         sleep(10)
         self.driver.find_element_by_xpath("//span[text()='Join now']").click()
         sleep(60*60)
+        print("Quitting...")
+        sleep(5)
         self.driver.quit()
         if self.count < 2:
             self.count = self.count + 1
         else:
             self.count = 0
+        self.findCount()
 
     def findClass(self):
         with open("schedule.csv","r") as csvFile:
@@ -56,15 +74,32 @@ class ClassAutomation():
         self.driver = webdriver.Firefox(profile)
         self.driver.get("https://accounts.google.com/")
         sleep(2)
-        self.driver.find_element_by_name("identifier").send_keys(username)
-        sleep(1)
-        self.driver.find_element_by_id("identifierNext").click()
-        sleep(3)
-        self.driver.find_element_by_name("password").send_keys(password)
-        sleep(1)
-        self.driver.find_element_by_id("passwordNext").click()
-        sleep(2)
+        try:
+            self.driver.find_element_by_name("identifier").send_keys(username)
+            sleep(1)
+        except:
+            self.driver.find_element_by_name("Email").send_keys(username)
+            sleep(1)
+        try: 
+            self.driver.find_element_by_id("identifierNext").click()
+            sleep(4)
+        except:
+            self.driver.find_element_by_id("next").click()
+            sleep(4)
+        try:
+            self.driver.find_element_by_name("password").send_keys(password)
+            sleep(1)
+        except:
+            self.driver.find_element_by_name("Passwd").send_keys(password)
+            sleep(1)
+        try:    
+            self.driver.find_element_by_id("passwordNext").click()
+            sleep(4)
+        except:
+            self.driver.find_element_by_id("trustDevice").click()
+            self.driver.find_element_by_id("submit").click()
+            sleep(4)
         self.driver.get("https://classroom.google.com/")
-        sleep(5)
+        sleep(6)
         
 ClassAutomation()
